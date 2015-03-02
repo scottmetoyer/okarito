04e8d474c6a8eec85ac12a4adecdc951612a8d6a
Scott Metoyer <scott.metoyer@gmail.com>
2015-03-01 18:33:39 -0800

    Added Ionic

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,48 +1,81 @@
-angular.module('starter.controllers', [])
+angular.module('okarito.controllers', ['okarito.services'])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
-  // Form data for the login modal
-  $scope.loginData = {};
+.controller('AppCtrl', function ($scope, $ionicModal, $timeout, authService) {
+    // Form data for the login modal
+    $scope.loginData = {};
+    $scope.loginData.email = 'scott.metoyer@gmail.com';
+    $scope.loginData.url = 'https://scottmetoyer.fogbugz.com';
+    $scope.loginData.password = '';
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
+    // Create the login modal that we will use later
+    $ionicModal.fromTemplateUrl('templates/login.html', {
+        scope: $scope,
+        hardwareBackButtonClose: false
+    }).then(function (modal) {
+        $scope.modal = modal;
 
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
+        // Check for a saved token - open login as needed
+        var token = authService.getToken();
+        if (token == null) {
+            $scope.login();
+        }
+    });
 
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
+    // Triggered in the login modal to close it
+    $scope.closeLogin = function () {
+        $scope.modal.hide();
+    };
 
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
+    // Open the login modal
+    $scope.login = function () {
+        $scope.modal.show();
+    };
 
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
+    // Perform the login action when the user submits the login form
+    $scope.doLogin = function () {
+        var root = $scope.loginData.url;
+        root = root.replace(/\/?$/, '/');
+
+        authService
+            .getApiUrl(root)
+            .then(function (response) {
+                var apiUrl = root + response.data.response.url;
+                window.localStorage.setItem('apiUrl', apiUrl);
+
+                return authService.loginUser(
+                    $scope.loginData.email,
+                    $scope.loginData.password,
+                    apiUrl)
+            })
+            .then(function (response) {
+                var token = response.data.response.token;
+                window.localStorage.setItem('token', token);
+
+                // We are logged in - close the login modal
+                $scope.closeLogin();
+            })
+            .catch(function (response) {
+                alert('There was an error logging in to FogBugz. Please check your entries and try again.');
+            });
+    };
+})
+.controller('CasesCtrl', function ($scope, dataService) {
+    $scope.filter = '';
+    $scope.cases = [];
+    $scope.apiUrl = window.localStorage.getItem('apiUrl');
+    $scope.token = window.localStorage.getItem('token');
+
+    var init = function () {
+        dataService
+            .getCases($scope.filter, $scope.apiUrl, $scope.token)
+            .then(function (response) {
+                $scope.cases = response.data.response.cases;
+            });
+    };
+
+    init();
 })
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-})
-
-.controller('PlaylistCtrl', function($scope, $stateParams) {
+.controller('CaseCtrl', function ($scope, $stateParams) {
+    $scope.case = { title: 'This is the big massive title of the case that we are reviewing if we can do this with', id: 4 };
 });
---
+++ www/js/services.js
@@ -0,0 +1,37 @@
+// Shared service utility functions
+function transform(data) {
+    var x2js = new X2JS();
+    var json = x2js.xml_str2json(data);
+    return json;
+}
+
+angular.module('okarito.services', [])
+
+.factory('authService', function ($http) {
+    return {
+        getToken: function () {
+            var token = window.localStorage.getItem("token");
+            return token;
+        },
+        getApiUrl: function (root) {
+            return $http.get(root + 'api.xml',
+                { transformResponse: transform });
+        },
+        loginUser: function (email, password, apiUrl) {
+            return $http.get(apiUrl + "cmd=logon&email=" + email + "&password=" + password,
+                    { transformResponse: transform });
+        }
+    }
+})
+
+.factory('dataService', function ($http) {
+    return {
+        getCase: function (id) {
+
+        },
+        getCases: function (filter, apiUrl, token) {
+            return $http.get(apiUrl + "cmd=search&q=" + filter + "&token=" + token,
+                    { transformResponse: transform });
+        }
+    }
+});
